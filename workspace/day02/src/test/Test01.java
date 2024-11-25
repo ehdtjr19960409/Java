a61df5c7f5f4e9e2c45f776fc864a365ce65ba85
ehdtjr19960409 <ehdtjr19960409@gmail.com>
2024-11-25 14:21:31 +0900

    241125 오전자료

---
+++ workspace/day02/src/test/Test01.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("신동석");
+		System.out.println(2024 + 1); 
+		//소괄호 안에 있는 2024 + 1값의 덧셈 연산결과를 콘솔창에 출력하고 줄바꿈한다
+	}
+}
