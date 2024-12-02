8fafb7bbd4ccf9c26cc203fcd0532b3cd56ab341
ehdtjr19960409 <ehdtjr19960409@gmail.com>
2024-12-03 08:36:41 +0900

    누락 자료 올리기

--- workspace/day08/src/Task/MethodTask.java
+++ workspace/day08/src/Task/MethodTask.java
@@ -1,5 +1,5 @@
 package Task;
-
+//day08과제
 public class MethodTask {
 	public static void main(String[] args) {
 		
@@ -97,10 +97,11 @@ public class MethodTask {
 	//해당 클래스 MethodTask mt = new MethodTask(); import
 	//syso getAvg(5개의 정수 넣기)
 	
-	double getAvg() {
+	double getAvg(int[] arr) {
+		arr = new int[5];
 		int sum =0;
 				
-		int[] arr = new arr[5];
+		
 		for(int i =0; i< arr.length; i++) {
 			
 			sum += arr[i];
@@ -116,18 +117,48 @@ public class MethodTask {
     // 5. 정수 배열 중 최대값과 최소값을 출력하는 메소드
     // 매개변수o(배열),리턴값 x
     // 메소드명 printMinMax
+	
+	// 리턴타입 : void
+	// 메소드명 : printMinMax
+	// 매개변수 : int[] ar1
+	// 
+	// 최소값 0번째방 기준
+	// 최대값 0번쨰방 기준
+	// for문 배열의 길이만큼 증가시킨다
+	// 최소값0번째 방에 있는 값과 배열 i+1번째 값과 비교 최소값 0번째 방값이 크다면
+	// 최소값 대입
+	// 거짓이면 최대값 0번째방과 비교 i+1번째 값이 더 크다면
+	// 최대값에 대입
 
+	// 해당 클래스 MethodTask mt = new MethodTask(); import
+	// syso(mt.printMinMax(?));
+	
+	
     // 6. 소문자는 대문자로, 대문자는 소문자로 바꿔주는 메소드
     // BaNanA -> bAnANa
     // 메소드명 : changeCase
     // 매개변수와 리턴값 자유 => 단, 형변환 이용할 것
+	// 리턴타입 : void
+	// 매개변수x
+	// BaNanA를 heap 메모리 참조값에 올리고 stack영역에 char 배열 참조변수명으로 생성한다
+	// for문 배열의길이만큼 증가시킨다
+	//  대문자 65 ~90 소문자 97~122이므로
+	// i번째방 값이 65보다 크거나 같고 90보다 작거나 같으면
+	// i번째방 값 + 32 
+	// i번째방 값이 97보다 크거나 같고 12보다 작거나 같으면
+	// i번째방 값 -32 
+	// 1차원 배열 출력하는 배열 for문 작성
+	// for 배열의 길이만큼 증가한다
+	// syso(배열명[j])
+	// main 메소드에서 mt.changCase();
+	
 
     // 7. 아이디와 비밀번호를 입력받아 로그인하기
     // 매개변수 o, 리턴값 o(boolean 타입)
     // 메소드명 : login
     // main메소드에서 id가 admin이고 비밀번호가 1234이면 관리자님 환영합니다 출력
     // 둘 중 하나라도 틀리면 잘못입력했습니다 출력
-
+	
 	
 }
 
