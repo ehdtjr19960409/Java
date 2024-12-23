4bafd5f7f420fd8629e75d5503b44f05b3b3be34
ehdtjr19960409 <ehdtjr19960409@gmail.com>
2024-12-24 00:34:42 +0900

    20241224 java 능단평2 1번 풀이

---
+++ workspace/day02/src/test/Test02.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		int cnt = 0 ;
+		  for(int i=1; i<5; i++){
+	            for(int k = 0; k<i; k++){
+	            	cnt ++;
+	                System.out.print(cnt);
+	            }
+	            System.out.println(" ");
+	            
+	        }
+		  
+		
+	
+	}
+}
